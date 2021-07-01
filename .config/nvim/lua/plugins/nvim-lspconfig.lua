3335a0a87e7d8eebfd6c735abc9058a394b6b898
Sonia Vasko <ksenija-vasjko@cookpad.com>
2021-07-01 23:51:08 +0100

    update nvim-lspconfig.lua

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -4,10 +4,18 @@ local on_attach = function(client)
     require('completion').on_attach(client)
 end
 
-nvim_lsp.solargraph.setup{{ on_attach=on_attach }}
-nvim_lsp.html.setup{{ on_attach=on_attach }}
-nvim_lsp.cssls.setup{{ on_attach=on_attach }}
-nvim_lsp.tsserver.setup{{ on_attach=on_attach }}
-nvim_lsp.jsonls.setup{}
-nvim_lsp.tailwindcss.setup{{ on_attach=on_attach }}
-nvim_lsp.yamlls.setup{{ on_attach=on_attach }}
+nvim_lsp.solargraph.setup{{ on_attach = on_attach }}
+nvim_lsp.html.setup{{ on_attach = on_attach }}
+nvim_lsp.cssls.setup{{ on_attach = on_attach }}
+nvim_lsp.tsserver.setup{{ on_attach = on_attach }}
+nvim_lsp.jsonls.setup{{ on_attach = on_attach }}
+nvim_lsp.yamlls.setup{{ on_attach = on_attach }}
+nvim_lsp.tailwindcss.setup{{ on_attach = on_attach }}
+nvim_lsp.stylelint_lsp.setup{
+  on_attach = on_attach,
+  settings = {
+    stylelintplus = {
+      autoFixOnSave = true
+    }
+  }
+}
