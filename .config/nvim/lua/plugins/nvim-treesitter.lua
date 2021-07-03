32a24e004957722dfd5002dca37e91bd4d42fe4e
Sonia Vasko <ksenija-vasjko@cookpad.com>
2021-07-03 10:40:21 +0100

    treesitter config

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,39 +1,17 @@
 vim.cmd('autocmd! bufnewfile,bufread *.jbuilder setlocal filetype=ruby')
 require('nvim-treesitter.configs').setup({
-    highlight = {
-        enable = true,
-    },
-    ensure_installed = 'maintained',
-    indent = {
-        enable = true,
-    },
-    incremental_selection = {
-        enable = true,
-        keymaps = {
-            init_selection = 'gnn',
-            node_incremental = 'grn',
-            scope_incremental = 'grc',
-            node_decremental = 'grm',
-        },
-    },
-    playground = {
-        enable = true,
-        disable = {},
-        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-        persist_queries = false, -- Whether the query persists across vim sessions
-    },
-    textobjects = {
-        select = {
-            enable = true,
-            keymaps = {
-                -- You can use the capture groups defined in textobjects.scm
-                ['af'] = '@function.outer',
-                ['if'] = '@function.inner',
-                ['ac'] = '@class.outer',
-                ['ic'] = '@class.inner',
-                ['aP'] = '@parameter.outer',
-                ['iP'] = '@parameter.inner',
-            },
-        },
-    },
+  indent = {
+    enable = true,
+  },
+  ensure_installed = {
+    'dockerfile',
+    'html',
+    'regex',
+    'scss',
+    'css',
+    'javascript',
+    'json',
+    'lua',
+    'yaml',
+  },
 })
