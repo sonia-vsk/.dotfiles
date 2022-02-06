e265ef18449d3355432903e32e0447a528c41fd0
Sonia Vasko <ksenija-vasjko@cookpad.com>
2022-02-06 19:31:06 +0000

    null-ls

---
+++ .config/nvim/lua/user/null-ls.lua
@@ -0,0 +1,27 @@
+local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+if not null_ls_status_ok then
+	return
+end
+
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+local formatting = null_ls.builtins.formatting
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+local diagnostics = null_ls.builtins.diagnostics
+
+null_ls.setup({
+	debug = false,
+	sources = {
+		formatting.prettier,
+		formatting.stylua,
+ 		formatting.rubocop,
+		diagnostics.rubocop,
+		on_attach = function(client)
+				vim.cmd([[
+            augroup LspFormatting
+                autocmd! * <buffer>
+                autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+            augroup END
+            ]])
+		end,
+	},
+})
