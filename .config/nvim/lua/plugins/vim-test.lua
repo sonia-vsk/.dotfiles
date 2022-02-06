0db77b09cb46167d3823ade8a59bd14118beeb1d
Sonia Vasko <ksenija-vasjko@cookpad.com>
2022-02-06 11:16:50 +0000

    remove old configs

--- .config/nvim/lua/plugins/vim-test.lua
+++
@@ -1,3 +0,0 @@
-vim.g["test#strategy"] = "vimux"
-vim.api.nvim_set_keymap("n", "<leader>r", ":TestFile<cr>", {noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>R", ":TestNearest<cr>", {noremap = true})
